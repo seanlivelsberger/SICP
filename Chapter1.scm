e4da29601999556d5fb13b199fc437e684926b17
Sean Livelsberger <slivelsberger@shoprunner.com>
2018-02-14 09:10:14 -0600

    Through 1.21

--- Chapter1.scm
+++ Chapter1.scm
@@ -411,3 +411,52 @@
   (cond ((= b 0) 0)
         ((even? b) (* (double a) (halve a)))
         (else (+ b (* a (- b 1))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 1.20.  The process that a procedure generates is of course
+; dependent on the rules used by the interpreter. As an example, consider the
+; iterative gcd procedure given above. Suppose we were to interpret this
+; procedure using normal-order evaluation, as discussed in section 1.1.5.
+; (The normal-order-evaluation rule for if is described in exercise 1.5.)
+; Using the substitution method (for normal order), illustrate the process
+; generated in evaluating (gcd 206 40) and indicate the remainder
+; operations that are actually performed. How many remainder operations
+; are actually performed in the normal-order evaluation of (gcd 206 40)?
+; In the applicative-order evaluation?
+
+; Normal Orders
+; (gcd 206 40)
+; (gcd 40 (remainder 206 40))
+; (gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+; ...
+
+; Applicative Order
+; (gcd 206 40)
+; (gcd 40 (remainder 206 40))
+; (gcd 40 6)
+; (gcd 6 (remainder 40 6))
+; (gcd 6 4)
+; (gcd 4 (remainder 6 4))
+; (gcd 4 2)
+; (gcd 2 (remainder 4 2))
+; (gcd 2 0)
+; 2
+; 4 remainder operations.
+
+
+; Exercise 1.21.  Use the smallest-divisor procedure to find the smallest
+; divisor of each of the following numbers: 199, 1999, 19999.
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(smallest-divisor 199)
+(smallest-divisor 1999)
+(smallest-divisor 19999)
